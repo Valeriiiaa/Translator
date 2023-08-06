83e25205125968a0dda30b9d1fda53dc221fc232
Valeriiiaa <113591597+Valeriiiaa@users.noreply.github.com>
2023-08-06 14:53:38 +0300

    fix voice chat

---
+++ Translator/Classes/Models/MessagesClasses/MessagesEmptyModel.swift
@@ -0,0 +1,17 @@
+//
+//  MessagesEmptyModel.swift
+//  Translator
+//
+//  Created by Valeriya Chernyak on 06.08.2023.
+//
+
+import Foundation
+
+class MessagesEmptyModel: BaseMessagesModel {
+    
+    let id: String
+    
+    init(id: String) {
+        self.id = id
+    }
+}
