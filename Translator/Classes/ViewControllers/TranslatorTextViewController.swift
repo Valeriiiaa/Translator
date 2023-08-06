40e472bd7b6b7ba7f98ca270d64ebe11109c2ccc
jarakys <chernovkirilldev@gmail.com>
2023-08-06 16:35:52 +0300

    Added SpeechManager

--- Translator/Classes/ViewControllers/TranslatorTextViewController.swift
+++ Translator/Classes/ViewControllers/TranslatorTextViewController.swift
@@ -11,6 +11,74 @@ import MLKitTranslate
 import IHProgressHUD
 import Combine
 
+import Speech
+
+enum SpeechManagerError: Error {
+    case noInput
+    case notSupported
+}
+
+class SpeechManager {
+    private let audioEngine: AVAudioEngine
+    private var speechRecognizer: SFSpeechRecognizer?
+    private var request: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    
+    init() {
+        audioEngine = AVAudioEngine()
+        requestTranscribePermissions()
+    }
+    
+    private func requestTranscribePermissions() {
+        SFSpeechRecognizer.requestAuthorization { [unowned self] authStatus in
+            DispatchQueue.main.async {
+                if authStatus == .authorized {
+                    print("Good to go!")
+                } else {
+                    print("Transcription permission was declined.")
+                }
+            }
+        }
+    }
+    
+    public func startRecognize(for language: String) throws {
+        guard let speechRecognizer = SFSpeechRecognizer(locale: .init(languageComponents: .init(identifier: language))) else {
+            throw SpeechManagerError.notSupported
+        }
+        guard speechRecognizer.isAvailable else {
+            throw SpeechManagerError.notSupported
+        }
+        self.speechRecognizer = speechRecognizer
+        let request = SFSpeechAudioBufferRecognitionRequest()
+        self.request = request
+        let node = audioEngine.inputNode
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat, block: { [weak self] buffer, _ in
+            guard let self else { return }
+            self.request?.append(buffer)
+        })
+        audioEngine.prepare()
+        try audioEngine.start()
+        
+        recognitionTask = speechRecognizer.recognitionTask(with: request, resultHandler: { [weak self] response, error in
+            guard let self else { return }
+            if let error {
+                print("[log] speech \(error.localizedDescription)")
+            }
+            guard let response else { return }
+            let text = response.bestTranscription.formattedString
+            print("[text] \(text)")
+        })
+    }
+    
+    public func stopRecognize() {
+        recognitionTask?.cancel()
+        audioEngine.stop()
+        speechRecognizer = nil
+        self.request = nil
+    }
+}
+
 class TranslatorTextViewController: UIViewController, UITextViewDelegate {
    
     @IBOutlet weak var translatedFlagImage: UIImageView!
@@ -30,6 +98,10 @@ class TranslatorTextViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var backgroundMainView: UIView!
     @IBOutlet weak var backgroundFlagSecondView: UIView!
     @IBOutlet weak var backgroundFlagFirstView: UIView!
+    
+    private lazy var speechManager: SpeechManager = {
+        .init()
+    }()
     
     private lazy var networkManager: NetworkManager = {
         NetworkManager()
@@ -219,6 +291,15 @@ class TranslatorTextViewController: UIViewController, UITextViewDelegate {
         pushSelectionScreen()
     }
    
+    @IBAction func speechToTextDidTap(_ sender: Any) {
+        let language = languageManager.originalLanguage.key.rawValue
+        do {
+            try speechManager.startRecognize(for: language)
+        } catch {
+            print("[log] speech \(error.localizedDescription)")
+        }
+    }
+    
     @IBAction func selectCountryDidTap(_ sender: Any) {
         pushSelectionScreen()
     }
